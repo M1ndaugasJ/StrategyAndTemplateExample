5431fbe56b16aa02735c1fb4da5ce656105ccf12
Mindaugas <ciamusu1@gmail.com>
2015-09-29 18:42:31 +0300

    deleted comments

--- src/GeneralClasses/TransportStation.java
+++ src/GeneralClasses/TransportStation.java
@@ -9,18 +9,6 @@ import TemplateExample.TrainEconomyTicketUrban;
 public class TransportStation {
 
     public static void main(String args[]) {
-//        Train train = new Train(new FreightTransport(), new EconomyClassTicket());
-//        System.out.println(train.calculateTrainTravelTime(4000, 100, Cities.Ignalina));
-//        System.out.println(train.trainDescription());
-//        System.out.println(train.sellTrainTicket());
-//
-//        Airplane airplane = new Airplane(new UrbanTransport());
-//        System.out.println(airplane.calculateTrainTravelTime(4000, 500, Cities.Nida));
-//        System.out.println(airplane.trainDescription());
-//
-//        TrainBussinessTicketUrban freightAirplaneTemplate = new TrainBussinessTicketUrban();
-//        freightAirplaneTemplate.travelDuration(7000,500,Cities.Mažeikiai);
-
         TrainEconomyTicketFreight trainEconomyTicketFreight = new TrainEconomyTicketFreight();
         System.out.println(trainEconomyTicketFreight.sellTicket());
 
--- src/TemplateExample/TrainWithTicket.java
+++ src/TemplateExample/TrainWithTicket.java
@@ -5,37 +5,4 @@ package TemplateExample;
  */
 public abstract class TrainWithTicket extends TransportWithTicket{
 
-//    private int ticketCount;
-//    private double ticketPrice;
-//
-//    final String ticketSold = "Ticket sold for %.2f";
-//    final String insufficientTickets = "Insufficient tickets.";
-//
-//    public abstract int getRemainingTickets();
-//
-//    public String sellTicket() {
-//        if(getRemainingTickets()!=0){
-//            ticketCount--;
-//            return String.format(ticketSold, getTicketPrice());
-//        }
-//        return String.format(insufficientTickets);
-//    }
-//
-//
-//    public int getTicketCount() {
-//        return ticketCount;
-//    }
-//
-//    public void setTicketCount(int ticketCount) {
-//        this.ticketCount = ticketCount;
-//    }
-//
-//    public double getTicketPrice() {
-//        return ticketPrice;
-//    }
-//
-//    public void setTicketPrice(double ticketPrice) {
-//        this.ticketPrice = ticketPrice;
-//    }
-
 }
